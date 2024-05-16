19ed04a007dd411cf6b8dd15b62984f33e57dd24
Troy Rice <troyanthonyrice@gmail.com>
2024-05-16 16:51:43 -0700

    cleaning code

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -8,38 +8,23 @@ const { validateUserId, validateUser, validatePost } = require('../middleware/mi
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
   User.get()
     .then(users => {
       res.json(users)
     })
-    .catch(err => {
-      res.status(500).json({
-        message: "User information could not be retrieved",
-        err: err.message
-      })
-    })
+    .catch(next)
 });
 
 router.get('/:id', validateUserId, async (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
-  try {
-    const user = await User.getById(req.params.id)
-      res.status(200).json(user)
-    }
-  catch (err) {
-    res.status(500).json({
-      message: "Problem finding user information",
-      err: err.message
-    })
-  }
-
+  res.json(req.user)
   })
   
 
-router.post('/', validateUser, async (req, res) => {
+router.post('/', validateUser, async (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
   try {
@@ -47,11 +32,7 @@ router.post('/', validateUser, async (req, res) => {
 
     res.status(201).json(newUser)
   }
-  catch (err) {
-    res.status(500).json({
-      message: "Problem posting new user"
-    })
-  }
+  catch{next}
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res) => {
@@ -99,6 +80,14 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessage: "something went wong inside posts router",
+    message: err.message,
+    stack: err.stack
+  })
+})
 
 // do not forget to export the router
 
